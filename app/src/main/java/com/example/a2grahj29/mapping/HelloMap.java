56b4ceaeb815325483281c72c7707785facffe50
2grahj29 <Altoids100>
2017-02-02 10:58:01 +0000

    "Added longitude and latitude EditText and Submit Button."

--- app/src/main/java/com/example/a2grahj29/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2grahj29/mapping/HelloMap.java
@@ -3,14 +3,17 @@ package com.example.a2grahj29.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 
 public class HelloMap extends Activity
+        implements View.OnClickListener
 {
-
     MapView mv;
 
     public void onCreate(Bundle savedInstanceState)
@@ -28,6 +31,23 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        //Submit button starts onClick event
+        Button submitButton = (Button) findViewById(R.id.submitbutton);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Retrieve the latitude
+        EditText latitudeEditText = (EditText) findViewById(R.id.latitudeEditText);
+        double latitude = Double.parseDouble(latitudeEditText.getText().toString());
+
+        //Retrieve the longitude
+        EditText longitudeEditText = (EditText) findViewById(R.id.longitudeEditText);
+        double longitude = Double.parseDouble(latitudeEditText.getText().toString());
+
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
     }
 }
 
